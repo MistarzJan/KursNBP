d4312048f523599ddb39cb24e4191ac76d3154a2
ajeffika <jan.mistarz6767@gmail.com>
2020-01-30 22:10:09 +0200

    added Morelinq, handled exceptions and printed output

--- NPBKurs/Program.cs
+++ NPBKurs/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace KursNBP
@@ -7,17 +10,82 @@ namespace KursNBP
     {
         static async Task Main()
         {
-            var currency = "EUR";
-            var startDate = "2019-01-01";
-            var endDate = "2019-12-12";
+            IDictionary<string, double> rate = new Dictionary<string, double>();
+            Console.WriteLine("Input Currency");
+            var currency = Console.ReadLine();
+            if (string.IsNullOrEmpty(currency))
+            {
+                Console.WriteLine("Currency can't be empty! Input your name once more");
+                currency = Console.ReadLine();
+            }
+            Console.WriteLine("Input Start Date");
+            var startDate = Console.ReadLine();
+            if (string.IsNullOrEmpty(startDate))
+            {
+                Console.WriteLine("Start Date can't be empty! Input your name once more");
+                startDate = Console.ReadLine();
+            }
+            Console.WriteLine("Input End Date");
+            var endDate = Console.ReadLine();
+            if (string.IsNullOrEmpty(endDate))
+            {
+                Console.WriteLine("End Date can't be empty! Input your name once more");
+                endDate = Console.ReadLine();
+            }
+            double sum = 0;
+            double average = 0;
+            double standardDeviationSum = 0;
+            double standardDeviation = 0;
+            double minimumValue = 0;
+            double maximumValue = 0;
+            var minDate = "";
+            var maxDate = "";
 
             ExchangeRateService exchangeRateService = new ExchangeRateService();
             exchangeRateService.SetVariables(currency, startDate, endDate);
             await exchangeRateService.GetExchangeRates();
-
-            foreach (ExchangeRate exchangeRate in exchangeRateService.ExchangeRatesData.ExchangeRates)
+            try
             {
-                Console.WriteLine(exchangeRate.Date);
+
+                foreach (ExchangeRate exchangeRate in exchangeRateService.ExchangeRatesData.ExchangeRates)
+                {
+                    rate.Add(new KeyValuePair<string, double>(exchangeRate.Date, exchangeRate.Value));
+                    sum += exchangeRate.Value;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Seems That You have entered invalid data, Please try again");
+                Main();
+            }
+
+            average = sum / rate.Count;
+
+            foreach (KeyValuePair<string, double> entry in rate)
+            {
+                standardDeviationSum += Math.Pow((entry.Value - average), 2);
+            }
+            standardDeviation = Math.Sqrt(standardDeviationSum / rate.Count);
+            try
+            {
+                 minimumValue = rate.Values.Min();
+                 maximumValue = rate.Values.Max();
+                 minDate = rate.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+                 maxDate = rate.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+                Console.WriteLine("");
+                Console.WriteLine($"{minDate} with value of: {minimumValue}");
+                Console.WriteLine($"{maxDate} with value of: {maximumValue}");
+                Console.WriteLine($"Average Rate: {average}");
+                Console.WriteLine($"Standad Deviation: {standardDeviation}");
+                Console.WriteLine($"Minimum Value: {minimumValue}");
+                Console.WriteLine($"Maximum Value: {maximumValue}");
+
+                Console.ReadLine();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Seems That You have entered invalid data, Please try again");
+                Main();
             }
         }
     }
